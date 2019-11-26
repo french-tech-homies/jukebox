d937d67aed6e322c89aadd2c9db752e3eaba0bdb
Yann Renaudin <renaudin.yann@gmail.com>
2019-11-26 16:28:29 -0500

    feat: add connection to Atlas for seeder

--- backend/src/storage/initDbConnection.ts
+++ backend/src/storage/initDbConnection.ts
@@ -1,15 +1,15 @@
-import mongoose from "mongoose";
-const credentials = { username: "", password: "" }; // TODO: move in .env file
-const CONNECTION_STRING = `mongodb+srv://${credentials.username}:${credentials.password}@cluster0-rdv9g.mongodb.net`;
+import mongoose from 'mongoose';
+const credentials = { username: '', password: '' }; // TODO: move in .env file
+const CONNECTION_STRING = `mongodb+srv://jukeboxdbuser:bpM7ZlIq49cgz8Ib@jukebox-nfppq.azure.mongodb.net/test?retryWrites=true&w=majority`;
 
 export async function startDbConnection() {
   const { connection } = await mongoose.connect(CONNECTION_STRING, {
-    dbName: "sandbox",
+    dbName: 'sandbox',
     bufferCommands: false,
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
   });
-  console.log("✅ Connection to MongoDB established");
+  console.log('✅ Connection to MongoDB established');
   return connection;
 }
