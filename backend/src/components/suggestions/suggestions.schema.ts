dc4ac8b7cde4e34632e7ebff6e2ff352d3c60b9a
Yann Renaudin <renaudin.yann@gmail.com>
2020-03-13 20:37:01 -0400

    feat: update suggestions and playlist queries, mutations and schema

--- backend/src/components/suggestions/suggestions.schema.ts
+++ backend/src/components/suggestions/suggestions.schema.ts
@@ -1,19 +1,10 @@
-import { objectType } from 'nexus';
+import { objectType } from 'nexus'
 
 export const Suggestion = objectType({
   name: 'Suggestion',
   definition(t) {
-    t.id('id');
-    t.string('submitter');
-    t.string('url');
-    t.field('artist', {
-      type: objectType({
-        name: 'Artist',
-        definition(t) {
-          t.string('name');
-          t.string('song');
-        },
-      }),
-    });
+    t.model.id()
+    t.model.submitter()
+    // t.model.track()
   },
-});
+})
